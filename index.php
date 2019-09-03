332642493bba146a75dfdc7a9620a7fad92d407c
rishabh169 <46092721+rishabh169@users.noreply.github.com>
2019-09-03 22:45:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/main.css">
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Sedgwick+Ave+Display" rel="stylesheet">
+
+</head>
+
+
+<body >
+	<div >
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top ">
+      
+      <div class="container">
+
+        <a href="index.php" class="navbar-brand text-light font-weight-bold ">Confirmo.</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target= "#collapsenavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+         <div class="collapse navbar-collapse text-center" id="collapsenavbar">
+
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="index.php" class="nav-link"> HOME </a>
+            </li>
+            <li class="nav-item">
+              <a href="php/signup.php" class="nav-link"> SIGN UP </a>
+            </li>
+
+              <li class="nav-item">
+              <a href="php/signin.php" class="nav-link"> LOGIN  </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="php/contactus.php" class="nav-link"> CONTACT US </a>
+            </li>
+
+            
+          </ul>  
+           
+         </div>
+
+        
+      </div>
+
+    </nav>
+		<div class="parallex1 text-center text-dark ">
+			<h1>Welcome to Confirmo.</h1>
+			<p>Book Your ticket easily</p>
+		</div>
+
+		<!-- <h1 id="main_head">You can easily find train live status</h1> -->
+
+		<div class="parallex2 text-light text-center">
+			<h1>Track train on single click.</h1>
+		</div>
+
+			<div class="parallex3 text-center text-white">
+			<h1>No More Waiting Ticket</h1>
+			<p>Our algorithm make your ticket CONFIRM</p>
+		</div>
+
+		<div class="parallex2 text-light text-center">
+			<h1>Welcome to your own rail Assistance</h1>
+		</div>
+		
+	</div>
+</body>
+</html>
